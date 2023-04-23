d3b642977d1f749bd8d3bddf27883b72946724c1
Kolade Amire <stephamire@gmail.com>
2023-04-23 21:31:23 +0100

    fix bug and Update Values.kt

--- src/main/kotlin/com/qlish/qlish/repository/Values.kt
+++ src/main/kotlin/com/qlish/qlish/repository/Values.kt
@@ -3,7 +3,7 @@ package com.qlish.qlish.repository
 class Values {
 
     val questionLevels = arrayOf("advanced", "intermediate", "elementary")
-    val vocabularyQuestionTopics = arrayOf("paragraph", "reading-comprehension", "synonyms", "phrasal-verbs","sentence-completion", "antonyms")
+    val vocabularyQuestionTopics = arrayOf("paragraph", "reading-comprehension", "synonyms", "phrasal-verb","sentence-completion", "antonyms")
     val grammarQuestionTopics = arrayOf("sentence-completion", "parts-of-speech", "tenses")
 
 }
